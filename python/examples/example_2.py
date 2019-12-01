aeafe85fc07fa4e36105f98ffec8bb44ba816a9f
Alvaro <alvgaona@gmail.com>
2019-11-30 22:30:25 -0300

    Init repo

---
+++ python/examples/example_2.py
@@ -0,0 +1,13 @@
+# Create dictionary with a person's data and print it.
+
+Persona = {
+  "nombre": "Lucas",
+  "edad": 27,
+  "dni": 123456789,
+  "padres": ["Claudia", "Fabian"]
+}
+
+print(Persona["nombre"])
+print(Persona["edad"])
+print(Persona["dni"])
+print(Persona["padres"])
